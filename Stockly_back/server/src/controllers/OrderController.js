6e337fea2d87e1e4c9be780ec8a123023655a397
Essi Chainer KOMISSA ZOTSU <essikomissa@gmail.com>
2024-05-06 18:32:56 +0100

    UpdateEtatCommande

--- Stockly_back/server/src/controllers/OrderController.js
+++ Stockly_back/server/src/controllers/OrderController.js
@@ -56,6 +56,19 @@ exports.updateCommandeById = async (req, res) => {
     }
 }
 
+exports.updateCommandeStateById = async (req, res) => {
+    const { id } = req.params;
+    const { etat } = req.body;
+    try {
+        const commande = await Commande.findByPk(id);
+        if (!commande) return res.status(404).json({ message: "La commande n'existe pas!" });
+        await commande.update({ etat : etat });
+        res.status(200).json({ message: "Commande modifiée avec succès!", commande });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
 exports.deleteCommandeById = async (req, res) => {
     const { id } = req.params;
     try {
--- Stockly_back/server/src/routes/OrderRoutes.js
+++ Stockly_back/server/src/routes/OrderRoutes.js
@@ -5,6 +5,7 @@ router.get('/', commandeController.getAllCommandes);
 router.get('/:id', commandeController.getCommandeById);
 router.post('/', commandeController.createCommande);
 router.put('/:id', commandeController.updateCommandeById);
+router.patch('/:id', commandeController.updateCommandeStateById);
 router.delete('/:id', commandeController.deleteCommandeById);
 
 module.exports = { commandeRouter: router };
